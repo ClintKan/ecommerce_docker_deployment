5732aa245e0db94f4d18173445a5f9ff20dd9c5c
Home <home@Homes-MBP.lan>
2024-11-20 02:23:16 -0500

    update

--- backend/my_project/settings_test.py
+++ backend/my_project/settings_test.py
@@ -5,6 +5,6 @@ from .settings import *  # Import all other settings
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db_test.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
